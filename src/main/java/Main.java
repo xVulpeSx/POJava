cd94663ad94b9cdb0536f2c9b8057e5b414c08f5
jlisek <jlisek@man.poznan.pl>
2021-06-16 18:35:41 +0200

    main update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import javax.swing.text.Style;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("HelloHere!");
-
+        System.out.println("HelloThere!");
+        new Menu().menu();
         System.out.println("General Kenobi.");
     }
 }
